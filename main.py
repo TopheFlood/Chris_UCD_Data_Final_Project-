3142a4ba171a54d087d635671a9a52b005fe3c1c
TopheFlood <flood.chris1@gmail.com>
2021-01-24 15:00:11 +0000

    checked data for missing values and made bar chart

--- main.py
+++ main.py
@@ -7,6 +7,22 @@ print(data.head())
 # to see the largest average price it was 3.25
 print(data["AveragePrice"].max())
 
+#checked for missing values
+print(data.isna())
+
+#checked by column
+print(data.isna().any())
+
+#imported matplotlib
+import matplotlib.pyplot as plt
+
+#Made barchart to confirm no missing data
+data.isna().sum().plot(kind="bar")
+
+#To show graph
+plt.show()
+#To save the graph
+plt.savefig()
 
 
 
