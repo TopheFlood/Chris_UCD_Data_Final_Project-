1b13f870382c7e2c329d0e87320ea722225ce387
TopheFlood <flood.chris1@gmail.com>
2021-01-24 14:45:25 +0000

    Checked the Max average price

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import pandas as pd
 
 data= pd.read_csv("avacados_data.csv")
-
+#First look at data
 print(data.head())
+
+# to see the largest average price it was 3.25
+print(data["AveragePrice"].max())
+
+
 
 
 
