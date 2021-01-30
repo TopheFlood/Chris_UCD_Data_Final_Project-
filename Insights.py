e48c946cd958377c22b21414b9b97485cab7becd
TopheFlood <flood.chris1@gmail.com>
2021-01-30 18:40:43 +0000

    Visulisation complete

---
+++ Insights.py
@@ -0,0 +1,5 @@
+#1 Organic avacods are more expensive
+#2 There was an increase in average price of both organic and conventional avacados in 2017
+#3 Conventional increased in average price in 2016 when organic saw a decrease
+#4 More conventional avacados are sold
+#5 conventional saw a big increase in volume between 2017 and 2018
--- Visualise.py
+++ Visualise.py
@@ -9,3 +9,35 @@ data.isna().sum().plot(kind="bar")
 
 #To save the graph
 #plt.savefig()
+
+sns.set_style("whitegrid")
+
+g= sns.relplot(x= "year",
+            y= "AveragePrice",
+            data= new_data,
+            kind= "line",
+            hue= "type",
+            size= "type",
+            ci= None)
+
+g.fig.suptitle("Avacados by type and average cost")
+g.set(xlabel="Year",
+       ylabel="Average price")
+
+plt.show()
+
+sns.set_style("whitegrid")
+
+g= sns.relplot(x= "year",
+            y= "Total Volume",
+            data= new_data,
+            kind= "line",
+            hue= "type",
+            size= "type",
+            ci= None)
+
+g.fig.suptitle("Avacados by type and total volume")
+g.set(xlabel="Year",
+       ylabel="Total volume")
+
+plt.show()
